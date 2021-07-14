5e7aaebcf135fc3efbea715707aa5b101c4ff903
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-14 16:19:04 +0300

    add test to create an item constructor

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 
-namespace ToDoList.TestTools
+namespace ToDoList.Tests
 {
   [TestClass]
   public class ItemsTests
   {
-    
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
   }
 }
