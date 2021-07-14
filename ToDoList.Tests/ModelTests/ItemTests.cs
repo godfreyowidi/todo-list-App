e1414327bbe2ccb7c3aab3051807d7940c2e5195
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-14 17:36:55 +0300

    add clearAll custom method to clear tests

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,16 +1,55 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemsTests
+  public class ItemsTests : IDisposable
   {
-    [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
+
+    public void Dispose()
     {
-      Item newItem = new Item();
+      Item.ClearAll();
+    }
+      [TestMethod]
+      public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("tests");
       Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+      [TestMethod]
+      public void GetDescription_ReturnDescription_String()
+    {
+      string description = "walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+
+      [TestMethod]
+      public void SetDescription_SetDescription_String()
+    {
+      string description = "walk the dog";
+      Item newItem = new Item(description);
+
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+      [TestMethod]
+      public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> {};
+
+      List<Item> result = Item.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
